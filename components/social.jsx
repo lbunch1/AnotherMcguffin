c03bc6cbf6039bd454e1930ebb5994d24b7a3b8a
bunch <bunch@pop-os.localdomain>
2022-08-13 22:05:04 -0700

    initial commit

---
+++ components/social.jsx
@@ -0,0 +1,6 @@
+export default function Social({ url, title }) {
+
+    return (
+        <a href={url}>{title}</a>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import Social from '../components/social'
+
+export default function Home() {
+  return (
+    <>
+      <Social url="#" title="ico" />
+    </>
+  )
+}
