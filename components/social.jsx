d83b223b4cc8341464e8aabe4dd8cc337af75a1f
bunch <bunch@pop-os.localdomain>
2022-08-14 21:45:23 -0700

    Socials functionality and style completed

--- components/social.jsx
+++ components/social.jsx
@@ -1,6 +1,14 @@
-export default function Social({ url, title }) {
+import Image from 'next/image';
+
+
+export default function Social({ url, img }) {
+
 
     return (
-        <a href={url}>{title}</a>
+        <a 
+        className="h-14 w-14 bg-zinc-400 rounded-full text-2xl font-bold text-gray-900 hover:scale-125"
+        href={url}>
+            <Image src={img} height={60} width={60} />
+            </a>
     )
 }
