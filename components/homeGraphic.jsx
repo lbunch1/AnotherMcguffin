fd9d0f7908644adb5f63f3d841afc99639caa14c
bunch <bunch@pop-os.localdomain>
2022-08-24 20:42:31 -0700

    added max width to homeGraphic component

--- components/homeGraphic.jsx
+++ components/homeGraphic.jsx
@@ -1,6 +1,6 @@
 const HomeGraphic = () => {
     return (
-        <div className="h-fit w-auto relative flex justify-center">
+        <div className="h-fit w-auto relative flex justify-center mx-auto lg:max-w-[60%]">
             <img src="/AMBanner_base.png" className="banner_base h-max-screen" alt={"Another McGuffin"} />
             <img src="/AMBanner_shine.png" className="banner_shine h-max-screen absolute" alt={"Another McGuffin"} />
         </div>
